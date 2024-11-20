0a1c279e67e8abaa2348b1a34d27f1be79da39c2
RushaanRajeshPai <pairushaan64@gmail.com>
2024-11-20 15:24:31 +0530

    quote done

---
+++ my-project/src/Components/Quotes/Quotes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Quotes = () => {
+  return (
+    <div>
+        <div className='container py-10 px-4'>
+            <h1 className='text-2xl sm:text-4xl font-bold max-w-[650px] mx-auto text-center font-serif'>
+                "Virtual reality is a medium, a means by which humans can share <span className='bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary'>ideas and experiences.</span>"
+            </h1>
+        </div>
+    </div>
+  )
+}
+
+export default Quotes
